e49c5e3e42a9d15112462eb25de32bfaf1abbc91
Ana Martínez Saucedo <anmartinez@uade.edu.ar>
2021-11-02 21:57:47 -0300

    UI, closes #6

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+from tkinter import Tk, Text, TOP, BOTH, X, N, LEFT
+from tkinter.constants import BOTTOM, CENTER, RIGHT
+from tkinter.ttk import Frame, Label, Entry, Button
+from ttkbootstrap import Style
 import numpy as np
 import matplotlib as mpl
 mpl.use("TkAgg")
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from utils.methods import calculate
 
 class CustomToolbar(NavigationToolbar2Tk):
     def __init__(self,canvas_,parent_):
@@ -18,31 +22,59 @@ class CustomToolbar(NavigationToolbar2Tk):
             ('Subplots', 'putamus parum claram', 'Configurar gráfico', 'configure_subplots'),
             ('Save', 'sollemnes in futurum', 'Guardar gráfico', 'save_figure'),
             )
-        NavigationToolbar2Tk.__init__(self,canvas_,parent_)
+        NavigationToolbar2Tk.__init__(self,canvas_,parent_, pack_toolbar=False)
 
 class App:
     def __init__(self, window):
         self.window = window
-        self._init_app()
+        self.place_ui_components()
+        self.draw_graph()
+     
 
-    def _init_app(self):
+    def place_ui_components(self):
+        self.window.title("Modelado y Simulación - TPO")
+        self.frame = Frame(self.window)
+        self.frame.pack(fill= BOTH, expand= True, padx= 20, pady=20)
+        self.frame.columnconfigure(2, weight=1)
+        Label(self.frame, text="Modelado y Simulación - TPO", font=('Helvetica', 16, 'bold')).grid(row=0, column=0, columnspan=2)
+        Label(self.frame, text="f(x,t)",style="primary.TLabel").grid(row=1, column=0 ,sticky="nsew")
+        self.function_input = Entry(self.frame, width=50).grid(pady=2, row=1, column=1)
+        Label(self.frame, text="t0",style="primary.TLabel").grid( row=2, column=0)
+        self.t0_input = Entry(self.frame, width=50).grid(pady=2, row=2, column=1)
+        Label(self.frame, text="x0",style="primary.TLabel").grid( row=3, column=0, sticky="nsew")
+        self.x0_input = Entry(self.frame, width=50).grid(pady=3, row=3, column=1)
+        Label(self.frame, text="tf",style="primary.TLabel").grid( row=4, column=0, sticky="nsew")
+        self.tf_input = Entry(self.frame, width=50).grid(pady=2, row=4, column=1)
+        Label(self.frame, text="h",style="primary.TLabel").grid( row=5, column=0, sticky="nsew")
+        self.h_input = Entry(self.frame, width=50).grid(pady=2, row=5, column=1)
+        Label(self.frame, text="N",style="primary.TLabel").grid( row=6, column=0, sticky="nsew")
+        self.n_input = Entry(self.frame, width=50).grid(pady=2, row=6, column=1)
+        Button(self.frame, text="Calcular", command=self.calculate, width=50).grid(pady=2, padx=5, row=7, column=0, columnspan=2)
+        
+    def calculate(self):
+        calculate(self.function_input.get(), "euler")
+
+    def draw_graph(self):
+        graph_frame = Frame(self.frame)
+        graph_frame.grid(row=1, column=2, rowspan=7)
+        graph_frame.columnconfigure(0, weight=1)
         self.figure = mpl.figure.Figure()
         self.ax = self.figure.add_subplot(111)
-        self.canvas = FigureCanvasTkAgg(self.figure,self.window )
-        self.toolbar = CustomToolbar(self.canvas,self.window )
+        self.canvas = FigureCanvasTkAgg(self.figure,graph_frame )
+        self.toolbar = CustomToolbar(self.canvas,graph_frame)
         self.toolbar.update()
-        self.plot_widget = self.canvas.get_tk_widget()
-        self.plot_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        self.toolbar.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.plot_widget = self.canvas.get_tk_widget().grid(row=1,column=0, sticky="nsew")
+        self.toolbar.grid(row=0, column=0)
         self.canvas.draw()
 
     # plot something random
     def plot(self):
-        self.ax.imshow(np.random.normal(0.,1.,size=[100,100]),cmap="hot",aspect="auto")
         self.figure.canvas.draw()
 
 def main():
-    root = tk.Tk()
+    style = Style(theme="minty")
+    style.configure('TLabel', font=('Helvetica', 12))
+    root = style.master
     app = App(root)
     app.plot()
     root.mainloop()
