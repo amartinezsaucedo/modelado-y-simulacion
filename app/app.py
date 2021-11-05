7d6b715c58ad7af6ac024530220407cbe3b3a091
Ana Martínez Saucedo <anmartinez@uade.edu.ar>
2021-11-05 19:26:00 -0300

    Finish app, closes #1, #3, #10

--- app/app.py
+++ app/app.py
@@ -5,58 +5,76 @@ from tkinter.ttk import Frame, Label, Entry, Button
 from ttkbootstrap import Style
 import numpy as np
 import matplotlib as mpl
-mpl.use("TkAgg")
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from utils.methods import calculate, calculate_h
 
+mpl.use("TkAgg")
+
+
 class CustomToolbar(NavigationToolbar2Tk):
-    def __init__(self,canvas_,parent_):
-        '''self.toolitems = (
-            ('Home', 'Reestablecer gráfico', 'home', 'home'),
-            ('Back', 'Mostrar gráfico anterior', 'back', 'back'),
-            ('Forward', 'Mostrar siguiente gráfico', 'forward', 'forward'),
-            (None, None, None, None),
-            ('Pan', 'Mover gráfico', 'move', 'pan'),
-            ('Zoom', 'dolore magna aliquam', 'Zoom en área', 'zoom'),
-            (None, None, None, None),
-            ('Subplots', 'putamus parum claram', 'Configurar gráfico', 'configure_subplots'),
-            ('Save', 'sollemnes in futurum', 'Guardar gráfico', 'save_figure'),
-            )'''
-        NavigationToolbar2Tk.__init__(self,canvas_,parent_, pack_toolbar=False)
+    def __init__(self, canvas_, parent_):
+        """self.toolitems = (
+        ('Home', 'Reestablecer gráfico', 'home', 'home'),
+        ('Back', 'Mostrar gráfico anterior', 'back', 'back'),
+        ('Forward', 'Mostrar siguiente gráfico', 'forward', 'forward'),
+        (None, None, None, None),
+        ('Pan', 'Mover gráfico', 'move', 'pan'),
+        ('Zoom', 'dolore magna aliquam', 'Zoom en área', 'zoom'),
+        (None, None, None, None),
+        ('Subplots', 'putamus parum claram', 'Configurar gráfico', 'configure_subplots'),
+        ('Save', 'sollemnes in futurum', 'Guardar gráfico', 'save_figure'),
+        )"""
+        NavigationToolbar2Tk.__init__(self, canvas_, parent_, pack_toolbar=False)
+
 
 class App:
     def __init__(self, window):
         self.window = window
         self.place_ui_components()
         self.draw_graph()
-     
 
     def place_ui_components(self):
         self.window.title("Modelado y Simulación - TPO")
         self.frame = Frame(self.window)
-        self.frame.pack(fill= BOTH, expand= True, padx= 20, pady=20)
+        self.frame.pack(fill=BOTH, expand=True, padx=20, pady=20)
         self.frame.columnconfigure(2, weight=1)
-        Label(self.frame, text="Modelado y Simulación - TPO", font=('Helvetica', 16, 'bold')).grid(row=0, column=0, columnspan=2)
-        Label(self.frame, text="f(x,t)",style="primary.TLabel").grid(row=1, column=0 ,sticky="nsew")
+        Label(
+            self.frame,
+            text="Modelado y Simulación - TPO",
+            font=("Helvetica", 16, "bold"),
+        ).grid(row=0, column=0, columnspan=2)
+        Label(self.frame, text="f(x,t)", style="primary.TLabel").grid(
+            row=1, column=0, sticky="nsew"
+        )
         self.function_input = Entry(self.frame, width=50)
         self.function_input.grid(pady=2, row=1, column=1)
-        Label(self.frame, text="t0",style="primary.TLabel").grid( row=2, column=0)
+        Label(self.frame, text="t0", style="primary.TLabel").grid(row=2, column=0)
         self.t0_input = Entry(self.frame, width=50)
         self.t0_input.grid(pady=2, row=2, column=1)
-        Label(self.frame, text="x0",style="primary.TLabel").grid( row=3, column=0, sticky="nsew")
+        Label(self.frame, text="x0", style="primary.TLabel").grid(
+            row=3, column=0, sticky="nsew"
+        )
         self.x0_input = Entry(self.frame, width=50)
         self.x0_input.grid(pady=3, row=3, column=1)
-        Label(self.frame, text="tf",style="primary.TLabel").grid( row=4, column=0, sticky="nsew")
+        Label(self.frame, text="tf", style="primary.TLabel").grid(
+            row=4, column=0, sticky="nsew"
+        )
         self.tf_input = Entry(self.frame, width=50)
         self.tf_input.grid(pady=2, row=4, column=1)
-        Label(self.frame, text="h",style="primary.TLabel").grid( row=5, column=0, sticky="nsew")
+        Label(self.frame, text="h", style="primary.TLabel").grid(
+            row=5, column=0, sticky="nsew"
+        )
         self.h_input = Entry(self.frame, width=50)
         self.h_input.grid(pady=2, row=5, column=1)
-        Label(self.frame, text="N",style="primary.TLabel").grid( row=6, column=0, sticky="nsew")
+        Label(self.frame, text="N", style="primary.TLabel").grid(
+            row=6, column=0, sticky="nsew"
+        )
         self.n_input = Entry(self.frame, width=50)
         self.n_input.grid(pady=2, row=6, column=1)
-        Button(self.frame, text="Calcular", command=self.calculate, width=50).grid(pady=2, padx=5, row=7, column=0, columnspan=2)
-        
+        Button(self.frame, text="Calcular", command=self.calculate, width=50).grid(
+            pady=2, padx=5, row=7, column=0, columnspan=2
+        )
+
     def calculate(self):
         f = self.function_input.get()
         t0 = float(self.t0_input.get())
@@ -68,8 +86,8 @@ class App:
             h = calculate_h(tf, t0, n)
         else:
             h = float(h)
-        f, x, t, y_true = calculate(f, x0, h , "euler")
-        self.plot(t, x, y_true)
+        t, x_euler, x_improved_euler, x_runge_kutta, exact = calculate(f, t0, tf, x0, h)
+        self.plot(t, x_euler, x_improved_euler, x_runge_kutta, exact)
 
     def draw_graph(self):
         graph_frame = Frame(self.frame)
@@ -77,33 +95,35 @@ class App:
         graph_frame.columnconfigure(0, weight=1)
         self.figure = mpl.figure.Figure()
         self.ax = self.figure.add_subplot(111)
-        self.canvas = FigureCanvasTkAgg(self.figure,graph_frame )
-        self.toolbar = CustomToolbar(self.canvas,graph_frame)
+        self.canvas = FigureCanvasTkAgg(self.figure, graph_frame)
+        self.toolbar = CustomToolbar(self.canvas, graph_frame)
         self.toolbar.update()
-        self.plot_widget = self.canvas.get_tk_widget().grid(row=1,column=0, sticky="nsew")
+        self.plot_widget = self.canvas.get_tk_widget().grid(
+            row=1, column=0, sticky="nsew"
+        )
         self.toolbar.grid(row=0, column=0)
         self.canvas.draw()
 
-    # plot something random
-    def plot(self, t, x, y_true):
+    def plot(self, t, x_euler, x_improved_euler, x_runge_kutta, exact):
         self.ax.clear()
-        self.ax.plot(t, x, 'bo--', label='Approximate')
-        self.ax.plot(t, y_true, 'g', label='Exact')
-        '''self.ax.plot(t, f(t ), 'g', label='Exact')
-        self.ax.title('Approximate and Exact Solution for Simple ODE')
-        self.ax.xlabel('t')
-        self.ax.ylabel('f(t)')
+        self.ax.plot(t, x_euler, "bo--", label="Euler")
+        self.ax.plot(t, x_improved_euler, "ro--", label="Euler mejorado (O(h^2))")
+        self.ax.plot(t, x_runge_kutta, "yo--", label="Runge-Kutta (O(h^4))")
+        self.ax.plot(t, exact, "g", label="Exacta")
+        self.ax.set_xlabel("t")
+        self.ax.set_ylabel("f(x, t)")
         self.ax.grid()
-        self.ax.legend(loc='lower right')
-        self.ax.show()'''
+        self.ax.legend(loc="lower right")
         self.figure.canvas.draw()
+
 
 def main():
     style = Style(theme="minty")
-    style.configure('TLabel', font=('Helvetica', 12))
+    style.configure("TLabel", font=("Helvetica", 12))
     root = style.master
     App(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- app/utils/methods.py
+++ app/utils/methods.py
@@ -3,21 +3,45 @@ from scipy.integrate import odeint
 import numpy as np
 
 
-def calculate(expression, x0, h ,method):
-    x, t = sym.symbols('x t')
-    f = sym.lambdify([x, t], expression) 
-    if method == "euler":
-        x, t = euler(f, x0, h)
-    y_true = odeint(f, x0, t)
-    return f, x, t, y_true
+def calculate(expression, t0, tf, x0, h):
+    x, t = sym.symbols("x t")
+    f = sym.lambdify([x, t], expression)
+    t_input = np.arange(t0, tf, h)
+    x_euler = euler(f, t_input, x0, h)
+    x_improved_euler = improved_euler(f, t_input, x0, h)
+    x_runge_kutta = runge_kutta(f, t_input, x0, h)
+    exact = odeint(f, x0, t_input)
+    return t_input, x_euler, x_improved_euler, x_runge_kutta, exact
+
 
 def calculate_h(tf, t0, n):
     return (tf - t0) / n
 
-def euler(f,x0, h):
-    t = np.arange(0, 1 + h, h)  
+
+def euler(f, t, x0, h):
     x = np.zeros(len(t))
     x[0] = x0
-    for k in range(0, len(t)- 1):
-        x[k + 1] = x[k] + h*f(x[k] ,t[k])
-    return x, t
+    for k in range(0, len(t) - 1):
+        x[k + 1] = x[k] + h * f(x[k], t[k])
+    return x
+
+
+def improved_euler(f, t, x0, h):
+    x = np.zeros(len(t))
+    x[0] = x0
+    for k in range(0, len(t) - 1):
+        predictor = x[k] + h * f(x[k], t[k])
+        x[k + 1] = x[k] + (h / 2) * (f(x[k], t[k]) + f(predictor, t[k + 1]))
+    return x
+
+
+def runge_kutta(f, t, x0, h):
+    x = np.zeros(len(t))
+    x[0] = x0
+    for k in range(0, len(t) - 1):
+        k1 = h * (f(x[k], t[k]))
+        k2 = h * (f(x[k] + 0.5 * k1, t[k] + h * 0.5))
+        k3 = h * (f(x[k] + 0.5 * k2, t[k] + h * 0.5))
+        k4 = h * (f(x[k] + k3, t[k] + h))
+        x[k + 1] = x[k] + (k1 + 2 * (k2 + k3) + k4) / 6.0
+    return x
